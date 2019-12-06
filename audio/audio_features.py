078c2ae0dc44732ce3aa1407039671a3da586851
Nikos Nikoloutsakos <tsakos.windsurfer@gmail.com>
2019-12-06 14:19:09 +0200

    Audio Features Extraction: dummy wav files

---
+++ audio/audio_features.py
@@ -0,0 +1,76 @@
+"""
+segment-based audio feature extraction
+"""
+
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from pyAudioAnalysis import MidTermFeatures as aF
+import utilities as ut
+
+"""
+It takes a list of paths as inputs and returns a list of feature matrices.
+
+Each resulting feature vector is extracted by long-term averaging the mid-term features.
+One feature vector is extracted for each WAV file.
+
+
+ARGUMENTS:
+    list_of_dirs:       list of paths of directories. Each directory
+                        contains a single audio class whose samples
+                        are stored in seperate WAV files.
+    mt_win, mt_step:    mid-term window length and step
+    st_win, st_step:    short-term window and step
+
+RETURNS:
+    features:
+    feature_names:
+    class_names:
+    file_names:
+    
+"""
+def audio_features_extraction(list_of_dirs,mt_win,mt_step,st_win,st_step,compute_beat):
+
+    class_names = [os.path.basename(d) for d in list_of_dirs]
+    features = []
+    file_names = []
+    for d in list_of_dirs:
+        f, file_names, feature_names = \
+            aF.directory_feature_extraction(d,mt_win,mt_step,st_win,st_step,compute_beat)
+        if f.shape[0] > 0:
+            features.append(f)
+            file_names.append(feature_names)
+            if d[-1] == os.sep:
+                class_names.append(d.split(os.sep)[-2])
+            else:
+                class_names.append(d.split(os.sep)[-1])
+
+    if len(features) == 0:
+        logging.error("ERROR: No data found in any input folder!")
+        return
+
+    return features, feature_names, class_names, file_names
+
+
+def main():
+
+    mt_win = 1.0
+    mt_step = 1.0
+    st_win = 0.050
+    st_step  = 0.050
+    compute_beat = False
+
+    dir_name = "../data/audio"
+    list_of_dirs = [os.path.join(dir_name, name)
+                    for name in os.listdir(dir_name)
+                    if os.path.isdir(os.path.join(dir_name, name))]
+
+    [features, feature_names, class_names, file_names ] = \
+            audio_features_extraction(list_of_dirs,mt_win,mt_step,st_win,st_step,compute_beat)
+
+    # uncomment for plot 
+    #ut.plot_feature_histograms(features, feature_names, class_names)
+
+if __name__ == '__main__':
+    main()
---
+++ audio/utilities.py
@@ -0,0 +1,52 @@
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_feature_histograms(list_of_feature_mtr, feature_names,
+                            class_names, n_columns=5):
+    '''
+    Plots the histograms of all classes and features for a given
+    classification task.
+    :param list_of_feature_mtr: list of feature matrices
+                                (n_samples x n_features) for each class
+    :param feature_names:       list of feature names
+    :param class_names:         list of class names, for each feature matr
+    '''
+    n_features = len(feature_names)
+    n_bins = 12
+    n_rows = int(n_features / n_columns) + 1
+    figs = plotly.tools.make_subplots(rows=n_rows, cols=n_columns,
+                                      subplot_titles=feature_names)
+    figs['layout'].update(height=(n_rows * 250))
+    clr = get_color_combinations(len(class_names))
+    for i in range(n_features):
+        # for each feature get its bin range (min:(max-min)/n_bins:max)
+        f = np.vstack([x[:, i:i + 1] for x in list_of_feature_mtr])
+        bins = np.arange(f.min(), f.max(), (f.max() - f.min()) / n_bins)
+        for fi, f in enumerate(list_of_feature_mtr):
+            # load the color for the current class (fi)
+            mark_prop = dict(color=clr[fi], line=dict(color=clr[fi], width=3))
+            # compute the histogram of the current feature (i) and normalize:
+            h, _ = np.histogram(f[:, i], bins=bins)
+            h = h.astype(float) / h.sum()
+            cbins = (bins[0:-1] + bins[1:]) / 2
+            scatter_1 = go.Scatter(x=cbins, y=h, name=class_names[fi],
+                                   marker=mark_prop, showlegend=(i == 0))
+            # (show the legend only on the first line)
+            figs.append_trace(scatter_1, int(i/n_columns)+1, i % n_columns+1)
+    for i in figs['layout']['annotations']:
+        i['font'] = dict(size=10, color='#224488')
+    plotly.offline.plot(figs, filename="report.html", auto_open=True)
+
+
+def get_color_combinations(n_classes):
+    clr_map = plt.cm.get_cmap('jet')
+    range_cl = range(int(int(255/n_classes)/2), 255, int(255/n_classes))
+    clr = []
+    for i in range(n_classes):
+        clr.append('rgba({},{},{},{})'.format(clr_map(range_cl[i])[0],
+                                              clr_map(range_cl[i])[1],
+                                              clr_map(range_cl[i])[2],
+                                              clr_map(range_cl[i])[3]))
+    return clr
