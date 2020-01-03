b854a5486ea4afde365197862d8099d1e8701023
Nikos Nikoloutsakos <tsakos.windsurfer@gmail.com>
2020-01-03 14:35:47 +0200

    Extract audio features per file using the index.csv

--- audio/audio_features.py
+++ audio/audio_features.py
@@ -3,74 +3,125 @@ segment-based audio feature extraction
 """
 
 import os
+import sys
+import glob
+import time
+import numpy as np
+import pandas as pd
 import logging
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
+#from pyAudioAnalysis import utilities
+from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import MidTermFeatures as aF
 import utilities as ut
 
 """
-It takes a list of paths as inputs and returns a list of feature matrices.
+This function extracts the mid-term features of the WAVE files from the index CSV.
+
+It takes a list of audio files as inputs and returns a list of feature matrices.
 
 Each resulting feature vector is extracted by long-term averaging the mid-term features.
 One feature vector is extracted for each WAV file.
 
 
 ARGUMENTS:
-    list_of_dirs:       list of paths of directories. Each directory
-                        contains a single audio class whose samples
-                        are stored in seperate WAV files.
-    mt_win, mt_step:    mid-term window length and step
-    st_win, st_step:    short-term window and step
+    dir_name:           Directory that contains the audio files
+    mt_win, mt_step:    mid-term window length and step (in seconds)
+    st_win, st_step:    short-term window and step (in seconds)
 
 RETURNS:
     features:
     feature_names:
-    class_names:
     file_names:
     
 """
-def audio_features_extraction(list_of_dirs,mt_win,mt_step,st_win,st_step,compute_beat):
+def audio_features_extraction(dir_name,mt_win,mt_step,st_win,st_step):
 
-    class_names = [os.path.basename(d) for d in list_of_dirs]
     features = []
     file_names = []
-    for d in list_of_dirs:
-        f, file_names, feature_names = \
-            aF.directory_feature_extraction(d,mt_win,mt_step,st_win,st_step,compute_beat)
-        if f.shape[0] > 0:
-            features.append(f)
-            file_names.append(feature_names)
-            if d[-1] == os.sep:
-                class_names.append(d.split(os.sep)[-2])
+
+    mid_term_features = np.array([])
+    process_times = []
+
+    # type is WAVE file, convert using the function video_to_audio.py
+    suffix = ".wav"
+    index_df = pd.read_csv(dir_name+'/'+'index.csv', sep=';')
+
+    wav_file_list, mid_feature_names = [], []
+
+    # iterate each audio file
+    for ind in index_df.index:
+        name = index_df['FILE'][ind]
+        seg = str(index_df['SEG'][ind])
+
+        file_path = dir_name + '/' + name + '/' + name + '_' + seg + suffix
+        print("Analyzing file {0:d} of {1:d}: {2:s}".format(ind+1,len(index_df),file_path))
+
+        if os.stat(file_path).st_size == 0:
+            logging.warning("WARNING: EMPTY FILE -- SKIPPING")
+            continue
+        [sampling_rate, signal] = audioBasicIO.read_audio_file(file_path)
+        if sampling_rate == 0:
+            logging.warning("WARNING: NO SAMPLING RATE -- SKIPPING")
+            continue
+
+        t1 = time.clock()
+        signal = audioBasicIO.stereo_to_mono(signal)
+        if signal.shape[0] < float(sampling_rate)/5:
+            logging.warning("WARNING: AUDIO FILE TOO SMALL -- SKIPPING")
+            continue
+        wav_file_list.append(file_path)
+
+        mid_features, _, mid_feature_names = \
+            aF.mid_feature_extraction(signal, sampling_rate,
+                                    round(mt_win * sampling_rate),
+                                    round(mt_step * sampling_rate),
+                                    round(st_win * sampling_rate),
+                                    round(st_step * sampling_rate))
+        mid_features = np.transpose(mid_features)
+        mid_features = mid_features.mean(axis=0)
+        # long term averaging of mid-term statistics
+        if (not np.isnan(mid_features).any()) and \
+                (not np.isinf(mid_features).any()):
+            if len(mid_term_features) == 0:
+                # append feature vector
+                mid_term_features = mid_features
             else:
-                class_names.append(d.split(os.sep)[-1])
+                mid_term_features = np.vstack((mid_term_features,mid_features))
 
-    if len(features) == 0:
-        logging.error("ERROR: No data found in any input folder!")
-        return
+        t2 = time.clock()
+        duration = float(len(signal)) / sampling_rate
+        process_times.append((t2 - t1) / duration)
 
-    return features, feature_names, class_names, file_names
+    if len(process_times) > 0:
+        print("Feature extraction complexity ratio: "
+                "{0:.1f} x realtime".format((1.0 /
+                                            np.mean(np.array(process_times)))))
 
+    print(mid_term_features.shape)
 
-def main():
+    ftr_df = pd.DataFrame(data=mid_term_features)
+    df=index_df.copy()
+    df=pd.concat([df,ftr_df], axis=1)
+    if True:
+        df.to_pickle('../data/Audio2Features.pkl')
+
+    return mid_term_features, wav_file_list, mid_feature_names
+
+def main(argv):
 
     mt_win = 1.0
     mt_step = 1.0
     st_win = 0.050
     st_step  = 0.050
-    compute_beat = False
 
-    dir_name = "../data/audio"
-    list_of_dirs = [os.path.join(dir_name, name)
-                    for name in os.listdir(dir_name)
-                    if os.path.isdir(os.path.join(dir_name, name))]
+    dir_name = "../data"
 
-    [features, feature_names, class_names, file_names ] = \
-            audio_features_extraction(list_of_dirs,mt_win,mt_step,st_win,st_step,compute_beat)
+    f, fn, feature_names = \
+        audio_features_extraction(dir_name,mt_win,mt_step,st_win,st_step)
 
-    # uncomment for plot 
-    #ut.plot_feature_histograms(features, feature_names, class_names)
+    #print(f,fn,feature_names)
 
 if __name__ == '__main__':
     main(sys.argv)
