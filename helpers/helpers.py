7ba455e5ee27aa1f738c6de6305ea1580d0a95d7
Konstantinos Dimitros <k.dimitros@gmail.com>
2020-02-11 12:38:36 +0200

    Helper function to concatenate feature dataframes - not implemented yet

---
+++ helpers/helpers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def merge_features(audio_pd, video_pd):
+    '''
+    Merging the audio & video dataframe into one single dataframe to be ready for training.
+
+    :param audio_pd: the audio features dataframe
+    :param video_pd: the video features dataframe
+    :return: the merged feature dataframe
+    '''
+
+
+
